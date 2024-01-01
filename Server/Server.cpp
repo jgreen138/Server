f4f72210d4548b18e090d67d41403b3febe03194
jgreen138 <30970944+jgreen138@users.noreply.github.com>
2024-01-02 01:02:11 +1300

    Client Server working stage 1
    Server is receiving file request and sending file correctly. Could use more user info while being used. Not been tested concurrently

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,5 +1,4 @@
 #undef UNICODE
-
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
@@ -8,15 +7,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Need to link with Ws2_32.lib
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
+#pragma comment(lib, "Ws2_32.lib")
 
 #define DEFAULT_BUFLEN 512
+#define FILE_BUFLEN 1024
 #define DEFAULT_PORT "27015"
 
-int __cdecl main(void)
-{
+// Function to send a file to the client
+int sendFile(SOCKET clientSocket, const char* fileName) {
+    // Construct the full path for the file based on the current executable's location
+    char fullPath[MAX_PATH];
+    if (GetModuleFileNameA(NULL, fullPath, MAX_PATH) == 0) {
+        printf("Error getting module filename\n");
+        return -1;
+    }
+
+    // Remove the filename from the full path
+    char* lastBackslash = strrchr(fullPath, '\\');
+    if (lastBackslash != NULL) {
+        *(lastBackslash + 1) = '\0';  // Null-terminate after the last backslash
+    }
+
+    // Append the filename to the path
+    strcat_s(fullPath, MAX_PATH, fileName);
+
+    FILE* file;
+    if (fopen_s(&file, fullPath, "rb") != 0) {
+        printf("Error opening file: %s\n", fullPath);
+        return -1;
+    }
+
+    char fileBuf[FILE_BUFLEN];
+    int bytesRead;
+
+    do {
+        bytesRead = fread(fileBuf, 1, sizeof(fileBuf), file);
+        if (bytesRead > 0) {
+            if (send(clientSocket, fileBuf, bytesRead, 0) == SOCKET_ERROR) {
+                printf("Error sending file to the client: %d\n", WSAGetLastError());
+                fclose(file);
+                return -1;
+            }
+        }
+    } while (bytesRead > 0);
+
+    fclose(file);
+    return 0;
+}
+
+int main(void) {
     WSADATA wsaData;
     int iResult;
 
@@ -89,35 +128,38 @@ int __cdecl main(void)
         return 1;
     }
 
-    // No longer need server socket
+    // No longer need the listening socket
     closesocket(ListenSocket);
 
     // Receive until the peer shuts down the connection
     do {
-
         iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
         if (iResult > 0) {
             printf("Bytes received: %d\n", iResult);
 
-            // Echo the buffer back to the sender
-            iSendResult = send(ClientSocket, recvbuf, iResult, 0);
-            if (iSendResult == SOCKET_ERROR) {
-                printf("send failed with error: %d\n", WSAGetLastError());
+            // Requested file name received from the client
+            recvbuf[iResult] = '\0';  // Null-terminate the received file name
+            printf("Requested file: %s\n", recvbuf);
+
+            // Send the file to the client
+            printf("Attempting to send file...\n");
+            if (sendFile(ClientSocket, recvbuf) == -1) {
+                printf("Error sending file to the client\n");
                 closesocket(ClientSocket);
                 WSACleanup();
                 return 1;
             }
-            printf("Bytes sent: %d\n", iSendResult);
+            printf("File sent successfully.\n");
         }
-        else if (iResult == 0)
+        else if (iResult == 0) {
             printf("Connection closing...\n");
+        }
         else {
             printf("recv failed with error: %d\n", WSAGetLastError());
             closesocket(ClientSocket);
             WSACleanup();
             return 1;
         }
-
     } while (iResult > 0);
 
     // shutdown the connection since we're done
@@ -135,3 +177,4 @@ int __cdecl main(void)
 
     return 0;
 }
+
